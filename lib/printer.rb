79aa493e191d8441457c48ba77d87179325aa50e
Raefe <raefe.nj@gmail.com>
2018-04-23 18:15:31 +0100

    added a print header method and a printer class

---
+++ lib/printer.rb
@@ -0,0 +1,8 @@
+require 'account'
+
+class Printer
+
+  def print_header
+    p 'date || credit || debit || balance'
+  end
+end
---
+++ spec/printer_spec.rb
@@ -0,0 +1,12 @@
+require 'printer'
+
+describe Printer do
+
+  subject(:printer) { described_class.new }
+
+  describe '.print_header' do
+    it 'should print a header once' do
+      expect(printer.print_header).to eq('date || credit || debit || balance')
+    end
+  end
+end
