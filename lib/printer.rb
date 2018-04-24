b8f0ea81265778593bf05256c55aad4763213c4c
Raefe <raefe.nj@gmail.com>
2018-04-24 18:10:55 +0100

    added functionality to the printer class

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,13 +2,28 @@ require_relative 'account.rb'
 
 class Printer
 
-  attr_reader :account
+  attr_reader :account, :array_of_transactions_with_header
 
   def initialize(account)
     @account = account
+    @array_of_transactions_with_header = []
   end
 
+  def array_formatter
+    @array_of_transactions_with_header.push(print_header)
+    @account.transaction_history.each do |y|
+      @array_of_transactions_with_header.push("#{y.date} || #{y.credit} || #{y.debit}")
+    end
+  end
+
+
+  def print_all_transactions
+    array_formatter
+    array_of_transactions_with_header.each { |x| p x}
+  end
+
+  private
   def print_header
-    p 'date || credit || debit || balance'
+    'date || credit || debit || balance'
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,11 +2,13 @@ require 'printer'
 
 describe Printer do
 
-  subject(:printer) { described_class.new('fake account') }
+  let(:deposit) {double :deposit, date: '10/10/10', debit: nil, credit: 42}
+  let(:account) {double :account, transaction_history: [deposit]}
+  subject(:printer) { described_class.new(account) }
 
-  describe '.print_header' do
-    it 'should print a header once' do
-      expect(printer.print_header).to eq('date || credit || debit || balance')
+  describe '.print_all_transactions' do
+    it 'should print a header once, and then all transactions' do
+      expect(printer.print_all_transactions).to eq(['date || credit || debit || balance', '10/10/10 || 42 || '])
     end
   end
 end
