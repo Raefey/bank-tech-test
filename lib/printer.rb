0cf61f6b87d500a32679de55906198be65043ce7
Raefe <raefe.nj@gmail.com>
2018-04-25 16:48:29 +0100

    only pass printer an array of transactions, instead of the whole account class

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,23 +2,22 @@ require_relative 'account.rb'
 
 class Printer
 
-  attr_reader :account, :array_of_transactions_with_header
+  attr_reader :array_of_transactions_with_header
 
-  def initialize(account)
-    @account = account
+  def initialize
     @array_of_transactions_with_header = []
   end
 
-  def array_formatter
+  def array_formatter(array_of_transactions)
     @array_of_transactions_with_header.push(print_header)
-    account.transaction_history.each do |y|
+    array_of_transactions.each do |y|
       @array_of_transactions_with_header.push("#{y.date} || #{y.credit} || #{y.debit} || #{y.balance_at_that_time}")
     end
   end
 
 
-  def print_all_transactions
-    array_formatter
+  def print_all_transactions(array_of_transactions)
+    array_formatter(array_of_transactions)
     array_of_transactions_with_header.each { |x| x }
   end
 
